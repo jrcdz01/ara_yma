98075af22e30e9a3c355a4ba0c711a7e51de9c12
jrcdz01 <jrcdz01@gmail.com>
2022-12-10 01:26:45 -0300

    atualização do sistema de saude e knockback

--- Assets/Scripts/HeartManager.cs
+++ Assets/Scripts/HeartManager.cs
@@ -25,9 +25,10 @@ public class HeartManager : MonoBehaviour
     }
 
     public void UpdateHearts(){
-        float tempHealth = playerCurrentHealth.initialValue/2;
+        float tempHealth = playerCurrentHealth.RuntimeValue/2;
+
         for (int i = 0; i <heartContainers.initialValue; i++){
-            if ( i<= tempHealth){
+            if ( i<= tempHealth - 1){
                 //full heart
                 hearts[i].sprite = fullHeart;
             }else if (i > tempHealth){
